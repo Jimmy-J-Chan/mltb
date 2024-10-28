c84060beecd7b0a07583d5f9c886130be029e3c1
Jimmy <jimmychan0507@gmail.com>
2024-10-28 14:23:39 +1000

    first commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="mltb",
+    version="2024.08.04",
+    author="Jimmy Chan",
+    author_email="jimmychan0507@gmail.com",
+    # packages=["mltb",
+    #           "model_selection",
+    #           "utils"],
+    description="Machine Learning ToolBox",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="",
+    license='MIT',
+    python_requires='>=3.10',
+    install_requires=['pandas',
+                      'numpy',
+                      'matplotlib',
+                      'PyYAML',
+                      'polars',
+                      'pyarrow',
+                      'scikit-learn',
+                      'lightgbm',
+                      'catboost',
+                      'xgboost']
+)
